1f0a0a4de7b9ad520f06cb594b7436e113910fe8
grigoriy-romanenko <grisha432@rambler.ru>
2015-02-26 17:38:32 +0200

    сделал добавление проекта в БД

--- src/main/java/dao/Utils.java
+++ src/main/java/dao/Utils.java
@@ -7,21 +7,23 @@ import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
+
 import java.util.Hashtable;
 
 public class Utils {
 
     private static final Logger LOGGER = Logger.getLogger(Utils.class);
+    private static String SERVER_URL = "t3://localhost:7001";
+    private static String DATASOURCE = "jndi_jdbc_ds";
 
     static DataSource getDataSource() throws NoConnectionException {
         Context context = null;
-        NoConnectionException exception = null;
         try {
             Hashtable ht = new Hashtable();
             ht.put(Context.INITIAL_CONTEXT_FACTORY, "weblogic.jndi.WLInitialContextFactory");
-            ht.put(Context.PROVIDER_URL, "t3://localhost:7001");
+            ht.put(Context.PROVIDER_URL, SERVER_URL);
             context = new InitialContext(ht);
-            DataSource dataSource = (DataSource) context.lookup("jndi_jdbc_ds");
+            DataSource dataSource = (DataSource) context.lookup(DATASOURCE);
             return dataSource;
         }
         catch (Exception e) {
