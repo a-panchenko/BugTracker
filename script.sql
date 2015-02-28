8beb42a135539cfb835ee117c998e5d3ad816ab8
grigoriy-romanenko <grisha432@rambler.ru>
2015-02-28 19:44:58 +0200

    добавил команды для создания таблицы reply в скрипт

--- script.sql
+++ script.sql
@@ -1,7 +1,9 @@
 DROP TABLE PROJECT;
 DROP TABLE ISSUE;
+DROP TABLE REPLY;
 DROP SEQUENCE proj_seq;
 DROP SEQUENCE issue_seq;
+DROP SEQUENCE reply_seq;
 
 CREATE TABLE PROJECT (
     project_id    NUMBER(10)     NOT NULL,
@@ -56,3 +58,28 @@ VALUES (3, 'Issue2', 'Description2', 'Priority2', 'Status2', '02/02/2015');
 
 INSERT INTO ISSUE (project_id, issue_title, description, priority, status, creation_date)
 VALUES (4, 'Issue3', 'Description3', 'Priority3', 'Status3', '02/01/2015');
+
+CREATE TABLE REPLY (
+    reply_id     NUMBER(10)       NOT NULL,
+    issue_id     NUMBER(10)       NOT NULL,
+    message      VARCHAR2(150)    NOT NULL,
+    post_date    DATE             NOT NULL,
+    CONSTRAINT reply_pk PRIMARY KEY (reply_id),
+    CONSTRAINT reply_fk FOREIGN KEY (issue_id) REFERENCES ISSUE(issue_id));
+    
+CREATE SEQUENCE reply_seq;
+
+CREATE OR REPLACE TRIGGER reply_seq_trg
+BEFORE INSERT ON REPLY
+FOR EACH ROW
+BEGIN
+    SELECT reply_seq.NEXTVAL
+    INTO :new.reply_id
+    FROM dual;
+END;
+
+INSERT INTO REPLY (issue_id, message, post_date)
+VALUES (3, 'Reply1', '02/01/2015');
+
+INSERT INTO REPLY (issue_id, message, post_date)
+VALUES (3, 'Reply2', '02/02/2015');
